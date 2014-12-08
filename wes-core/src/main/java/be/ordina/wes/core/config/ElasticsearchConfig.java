2cb457f23775c77a21f78a0e6958cf88d2382ec9
Tom Causs <tom.causs@ordina.be>
2014-12-08 17:03:47 +0100

    Set Elasticsearch as a search-only node

--- wes-core/src/main/java/be/ordina/wes/core/config/ElasticsearchConfig.java
+++ wes-core/src/main/java/be/ordina/wes/core/config/ElasticsearchConfig.java
@@ -2,9 +2,11 @@ package be.ordina.wes.core.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource("elasticsearch.properties")
 public class ElasticsearchConfig {
 
 	private static final String ES_INDEX_NAME = "elasticsearch.index.name";
--- wes-core/src/main/java/be/ordina/wes/core/config/MainConfig.java
+++ wes-core/src/main/java/be/ordina/wes/core/config/MainConfig.java
@@ -3,13 +3,11 @@ package be.ordina.wes.core.config;
 import org.elasticsearch.client.Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import be.ordina.wes.core.service.ClientFactory;
 import be.ordina.wes.core.service.ClientFactoryImpl;
 
 @Configuration
-@PropertySource("classpath:elasticsearch.properties")
 public class MainConfig {
 
     @Bean
