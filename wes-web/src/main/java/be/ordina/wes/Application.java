8f45168c12dc75f12a5d9e93ad430fce3e756f10
Tom Causs <tom.causs@ordina.be>
2014-12-09 12:38:55 +0100

    Disable Spring Boot auto configuration for Elasticsearch

--- wes-web/src/main/java/be/ordina/wes/Application.java
+++ wes-web/src/main/java/be/ordina/wes/Application.java
@@ -2,12 +2,13 @@ package be.ordina.wes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "be.ordina.wes")
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {ElasticsearchAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
