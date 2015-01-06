9b478d48b14145a4161baa38d88944ed5a2aae91
Tom Causs <tom.causs@ordina.be>
2015-01-06 12:28:15 +0100

    Add UT for checking imported data

--- wes-core/src/test/java/be/ordina/wes/core/service/ClientFactoryTest.java
+++ wes-core/src/test/java/be/ordina/wes/core/service/ClientFactoryTest.java
@@ -16,13 +16,35 @@ import be.ordina.wes.core.config.TestConfig;
 @ContextConfiguration(classes = { TestConfig.class })
 public class ClientFactoryTest {
 
+	private static final String IMPORTED_INDEX = "inventory";
+	
+	private final int expectedDocumentCount = 1217;
+	
 	@Autowired
 	private Client client;
 	
+	/**
+	 * Test that we can connect to the cluster and that it's ready (cluster health = green).
+	 * 
+	 * Red status indicates that a shard is not allocated in the cluster, 
+	 * yellow means that the primary shard is allocated but replicas are not, 
+	 * and green means that all shards are allocated.
+	 */
 	@Test
 	public void testClusterState() {
 		ClusterHealthResponse clusterHealth = client.admin().cluster().prepareHealth().get();
 		
 		Assert.assertEquals(ClusterHealthStatus.GREEN, clusterHealth.getStatus());
+	}
+	
+	/**
+	 * Test that all the test data was imported
+	 */
+	@Test
+	public void testImportedData() {
+		Assert.assertTrue(client.admin().indices().prepareExists(IMPORTED_INDEX).get().isExists());
+		
+		long actualDocumentCount = client.prepareCount(IMPORTED_INDEX).get().getCount();
+		Assert.assertEquals(expectedDocumentCount, actualDocumentCount);
 	}
 }
