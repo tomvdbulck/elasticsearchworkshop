8edf99a8752bcc481593c9118de344d91cda38ba
Tom Causs <tom.causs@ordina.be>
2015-01-12 15:46:45 +0100

    add exercise #1

--- wes-exercises/src/main/java/be/ordina/wes/exercises/Exercise1.java
+++ wes-exercises/src/main/java/be/ordina/wes/exercises/Exercise1.java
@@ -1,5 +1,45 @@
 package be.ordina.wes.exercises;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Exercise #1: 
+ * Connecting to Elasticsearch cluster
+ */
 public class Exercise1 {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Exercise1.class);
+	
+	private static final String SERVER_HOST = "192.168.33.10";
+	private static final int SERVER_PORT = 9300;
+	private static final String CLUSTER_NAME = "ordina-dev-001";
+	private static final String NODE_NAME = "ordiNode";
+	
+	private static Client client;
+	
+	public static Client getInstance() {
+		if (client == null) {
+			final Settings settings = ImmutableSettings.settingsBuilder()
+					.put("cluster.name", CLUSTER_NAME)
+	                .put("node.name", NODE_NAME)
+					.build();
+			
+			client = new TransportClient(settings)
+					.addTransportAddress(new InetSocketTransportAddress(SERVER_HOST, SERVER_PORT));
+			
+			LOG.debug("Starting up Elasticsearch transport client");
+		}
+		return client;
+	}
+	
+	public static void destroyInstance() {
+		client.close();
+	}
 
 }
--- wes-exercises/src/test/java/be/ordina/wes/exercises/Exercise1Test.java
+++ wes-exercises/src/test/java/be/ordina/wes/exercises/Exercise1Test.java
@@ -1,5 +1,41 @@
 package be.ordina.wes.exercises;
 
+import java.util.concurrent.ExecutionException;
+
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthStatus;
+import org.elasticsearch.client.Client;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class Exercise1Test {
 
+	private static Client client;
+	
+	@BeforeClass
+	public static void setUp() {
+		client = Exercise1.getInstance();
+	}
+	
+	@AfterClass
+	public static void tearDown() {
+		Exercise1.destroyInstance();
+	}
+	
+	/**
+	 * Test that we can connect to the cluster and that it's ready (i.e. cluster health = green).
+	 * 
+	 * Red status indicates that a shard is not allocated in the cluster, 
+	 * yellow - primary shard is allocated but replicas are not, 
+	 * green - all shards are allocated.
+	 */
+	@Test
+	public void testClusterState() throws InterruptedException, ExecutionException {
+		ClusterHealthResponse clusterHealth = client.admin().cluster().prepareHealth().get();
+		
+		Assert.assertEquals(ClusterHealthStatus.GREEN, clusterHealth.getStatus());
+	}
+	
 }
