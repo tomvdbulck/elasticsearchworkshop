d6441710ba2ed9f899ca6059c1d9cae06e61d02c
Tom Causs <tom.causs@ordina.be>
2015-01-13 16:45:25 +0100

    add exercise #2

---
+++ wes-exercises/src/main/java/be/ordina/wes/exercises/Exercise2.java
@@ -0,0 +1,53 @@
+package be.ordina.wes.exercises;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.client.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Exercise #2:
+ * Creating index & indexing data
+ */
+public class Exercise2 {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Exercise2.class);
+	
+	private static final String PERSON_FILE = "../sample_data/person.json";
+	private static final String TWITTER_FILE = "../sample_data/tweet.json";
+	private static final String TWITTER_INDEX = "twitter";
+	private static final String TWEET_TYPE = "tweet";
+	
+	private static Client client = Exercise1.getInstance();
+	
+	public static void createIndex() {
+		client.admin().indices().prepareCreate(TWITTER_INDEX).get();
+	}
+	
+	public static void indexOneDocument() throws IOException {
+		// read data from JSON file
+		String jsonDocument = new String(Files.readAllBytes(Paths.get(TWITTER_FILE)));
+		
+		client.prepareIndex(TWITTER_INDEX, TWEET_TYPE)
+				.setSource(jsonDocument)
+				.get();
+
+		LOG.info("indexing '{}' type object into '{}' index", TWEET_TYPE, TWITTER_INDEX);
+	}
+	
+	public static void indexMultipleDocuments() throws Exception {
+		// read data from JSON file
+		byte[] jsonData = Files.readAllBytes(Paths.get(PERSON_FILE));
+		
+		BulkRequestBuilder bulkRequest = client.prepareBulk();
+		bulkRequest.add(jsonData, 0, jsonData.length, true);
+        
+		// perform the bulk operation
+		bulkRequest.get();
+	}
+	
+}
---
+++ wes-exercises/src/test/java/be/ordina/wes/exercises/Exercise2Test.java
@@ -0,0 +1,70 @@
+package be.ordina.wes.exercises;
+
+import java.io.IOException;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Exercise2Test {
+	
+	private static final String PERSON_INDEX = "person";
+	private static final String TWITTER_INDEX = "twitter";
+
+	private static Client client;
+	
+	private int expectedPersonDocuments = 5000;
+	
+	@BeforeClass
+	public static void setUp() {
+		client = Exercise1.getInstance();
+	}
+	
+	@AfterClass
+	public static void tearDown() {
+		// delete all indices when we're done with tests
+		client.admin().indices().prepareDelete(PERSON_INDEX, TWITTER_INDEX).get();
+		
+		Exercise1.destroyInstance();
+	}
+	
+	@Test
+	public void testCreateIndex() {
+		Exercise2.createIndex();
+		
+		boolean indexExists = client.admin().indices().prepareExists(TWITTER_INDEX).get().isExists();
+		Assert.assertTrue(indexExists);
+	}
+	
+	@Test
+	public void testIndexOneDocument() throws IOException {
+		Exercise2.indexOneDocument();
+		
+		// refresh the index prior to performing search operations
+		client.admin().indices().prepareRefresh().get();
+		
+		SearchResponse response = client.prepareSearch(TWITTER_INDEX).get();
+		long resultCount = response.getHits().getTotalHits();
+		
+		Assert.assertEquals(1, resultCount);
+	}
+	
+	@Test
+	public void testIndexMultipleDocuments() throws Exception {
+		Exercise2.indexMultipleDocuments();
+		
+		boolean indexExists = client.admin().indices().prepareExists(PERSON_INDEX).get().isExists();
+		Assert.assertTrue(indexExists);
+		
+		// refresh the index prior to performing search operations
+		client.admin().indices().prepareRefresh().get();
+		
+		long docCount = client.prepareCount(PERSON_INDEX).get().getCount();
+		
+		Assert.assertEquals(expectedPersonDocuments, docCount);
+	}
+	
+}
