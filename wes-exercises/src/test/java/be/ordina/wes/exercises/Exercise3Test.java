042f324c3a3ffb35f7bfb07a72a1edc288e4ad3e
Tom Causs <tom.causs@ordina.be>
2015-01-15 15:03:05 +0100

    delete index when UT is finished

--- wes-exercises/src/test/java/be/ordina/wes/exercises/Exercise3Test.java
+++ wes-exercises/src/test/java/be/ordina/wes/exercises/Exercise3Test.java
@@ -2,14 +2,32 @@ package be.ordina.wes.exercises;
 
 import java.util.List;
 
+import org.elasticsearch.client.Client;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import be.ordina.wes.exercises.model.Person;
 
 public class Exercise3Test {
 
+	private static final String PERSON_INDEX = "person";
+
+	private static Client client;
+	
 	private final int expectedResultCount = 14;
+	
+	@BeforeClass
+	public static void setUp() {
+		client = Exercise1.getInstance();
+	}
+	
+	@AfterClass
+	public static void tearDown() {
+		// delete the index when we're done with tests
+		client.admin().indices().prepareDelete(PERSON_INDEX).get();
+	}
 	
 	/**
 	 * Test searching functionality
